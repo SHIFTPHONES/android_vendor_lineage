c07b487aaf709adf0ef2561af02ea357d13890f8
Dominik Baronelli <dbaronelli@shiftphones.com>
2023-05-15 11:45:57 +0200

    config: sdk: Add org.lineageos.services
    Change-Id: I4b9f42b1f19d57ed1299228296351fd0aab1206d
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- config/lineage_sdk_common.mk
+++ config/lineage_sdk_common.mk
@@ -9,15 +9,19 @@ PRODUCT_COPY_FILES += \
 # Lineage Platform Library
 PRODUCT_PACKAGES += \
     org.lineageos.platform-res \
-    org.lineageos.platform
+    org.lineageos.platform \
+    org.lineageos.services
 
 # AOSP has no support of loading framework resources from /system_ext
 # so the SDK has to stay in /system for now
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/framework/oat/%/org.lineageos.platform.odex \
     system/framework/oat/%/org.lineageos.platform.vdex \
+    system/framework/oat/%/org.lineageos.services.odex \
+    system/framework/oat/%/org.lineageos.services.vdex \
     system/framework/org.lineageos.platform-res.apk \
-    system/framework/org.lineageos.platform.jar
+    system/framework/org.lineageos.platform.jar \
+    system/framework/org.lineageos.services.jar
 
 ifndef LINEAGE_PLATFORM_SDK_VERSION
   # This is the canonical definition of the SDK version, which defines
